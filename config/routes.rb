0191ba0b54f4f6872088b16096706affd73c6ab6
KaemonL <kaemonlovendahl14@hotmail.com>
2018-08-26 20:08:42 -0600

    Added basic mailer setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root "static_pages#home"
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :account_activation, only: [:edit]
 end
